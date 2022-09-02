b7c0a1320c557f829f746e75f8f518dabc2bd29e
SquareHammer89 <pythonsanchez@gmail.com>
2022-09-02 15:52:14 -0700

    testing graph with matplotlib

--- standings.py
+++ standings.py
@@ -1,5 +1,7 @@
+import matplotlib.pyplot as plt
 from config import rapid_api
 import pandas as pd
+import numpy as np
 import requests
 import json
 
@@ -23,6 +25,7 @@ team_list = []
 wins_list = []
 draws_list = []
 loses_list = []
+points_list = []
 
 count = 0
 while count < 20:
@@ -31,15 +34,24 @@ while count < 20:
     wins = int(json.dumps(json_res["response"][0]["league"]["standings"][0][count]["all"]["win"]))
     draws = int(json.dumps(json_res["response"][0]["league"]["standings"][0][count]["all"]["draw"]))
     loses = int(json.dumps(json_res["response"][0]["league"]["standings"][0][count]["all"]["lose"]))
+    points = int(json.dumps(json_res["response"][0]["league"]["standings"][0][count]["points"]))
     rank_list.append(rank)
     team_list.append(team)
     wins_list.append(wins)
     draws_list.append(draws)
     loses_list.append(loses)
+    points_list.append(points)
     count += 1
 
-zipped = list(zip(rank_list, team_list, wins_list, draws_list, loses_list))
+zipped = list(zip(rank_list, team_list, wins_list, draws_list, loses_list, points_list))
 
-df = pd.DataFrame(zipped, columns=['Rank', 'Team', 'Wins', 'Draws', 'Loses'])
+df = pd.DataFrame(zipped, columns=['Rank', 'Team', 'Wins', 'Draws', 'Loses', 'Points'])
 
-print(df)
+df_graph = pd.DataFrame({
+    'Points': points_list,
+    'Wins': wins_list,
+    'Draws': draws_list
+    }, index=team_list)
+
+ax = df_graph.plot.bar(rot=45)
+plt.show()
