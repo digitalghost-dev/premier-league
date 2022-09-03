afb72252f7535dcaf75c343dcff277b765ceba29
SquareHammer89 <pythonsanchez@gmail.com>
2022-09-02 20:22:36 -0700

    removing imports, calling class from standings.py

--- main.py
+++ main.py
@@ -1,23 +1,8 @@
-from config import rapid_api
-import requests
-import json
- 
-url = "https://api-football-v1.p.rapidapi.com/v3/fixtures"
- 
-# League 135 is Italy
-querystring = {"league":"135","season":"2022"}
- 
-headers = {
-    "X-RapidAPI-Key": rapid_api,
-    "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com"
-}
- 
-response = requests.request("GET", url, headers=headers, params=querystring)
- 
-json_res = response.json()
- 
-game_1 = json.dumps(json_res["response"][0], indent=4)
-game_2 = json.dumps(json_res["response"][1], indent=4)
-game_3 = json.dumps(json_res["response"][1], indent=4)
- 
-print(game_1, game_2, game_3)
+def stats():
+    from standings import Standings
+    standings = Standings()
+
+    return standings.table(), standings.graph()
+
+if __name__ == "__main__":
+    stats()
