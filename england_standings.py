4e44890b1a6d89dc1653e8f48e140bcb11424023
SquareHammer89 <pythonsanchez@gmail.com>
2022-09-17 12:18:06 -0700

    moving england_standings out of standings folder

---
+++ england_standings.py
@@ -0,0 +1,74 @@
+from config import table_name, rapid_api, project_id
+from google.cloud import bigquery
+import pandas as pd
+import numpy as np
+import requests
+import json
+
+url = "https://api-football-v1.p.rapidapi.com/v3/standings"
+
+querystring = {"season":"2022","league":"39"}
+
+headers = {
+	"X-RapidAPI-Key": rapid_api,
+	"X-RapidAPI-Host": "api-football-v1.p.rapidapi.com"
+}
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+json_res = response.json()
+
+rank_list = []
+team_list = []
+wins_list = []
+draws_list = []
+loses_list = []
+points_list = []
+
+count = 0
+while count < 20:
+    rank_list.append(int(json.dumps(json_res["response"][0]["league"]["standings"][0][count]["rank"])))
+    team_list.append(str(json.dumps(json_res["response"][0]["league"]["standings"][0][count]["team"]["name"])))
+    wins_list.append(int(json.dumps(json_res["response"][0]["league"]["standings"][0][count]["all"]["win"])))
+    draws_list.append(int(json.dumps(json_res["response"][0]["league"]["standings"][0][count]["all"]["draw"])))
+    loses_list.append(int(json.dumps(json_res["response"][0]["league"]["standings"][0][count]["all"]["lose"])))
+    points_list.append(int(json.dumps(json_res["response"][0]["league"]["standings"][0][count]["points"])))
+    count += 1
+
+class engStandings:
+
+    def drop(self):
+        client = bigquery.Client()
+        query = """
+            DROP TABLE 
+            {}.european_football_leagues.england
+        """.format(project_id)
+
+        query_job = client.query(query)
+
+        print("Table dropped...")
+
+    def table(self):
+        headers = ['Rank', 'Team', 'Wins', 'Draws', 'Loses', 'Points']
+        zipped = list(zip(rank_list, team_list, wins_list, draws_list, loses_list, points_list))
+
+        df = pd.DataFrame(zipped, columns=headers)
+
+        # Construct a BigQuery client object.
+        client = bigquery.Client(project=project_id)
+
+        table_id = table_name
+
+        job = client.load_table_from_dataframe(
+            df, table_id
+        )  # Make an API request.
+        job.result()  # Wait for the job to complete.
+
+        table = client.get_table(table_id)  # Make an API request.
+        print(
+            "Loaded {} rows and {} columns to {}".format(
+                table.num_rows, len(table.schema), table_id
+            )
+        )
+
+        print(df)
