7b0249d2066515cb96c9027314ad811ac5eba206
SquareHammer89 <pythonsanchez@gmail.com>
2022-11-15 13:26:28 -0800

    implementing OOP through the whole file

--- src/standings.py
+++ src/standings.py
@@ -6,70 +6,89 @@ import numpy as np
 import requests
 import json
 
-# Headers used for RapidAPI.
-headers = {
-	"X-RapidAPI-Key": rapid_api,
-	"X-RapidAPI-Host": "api-football-v1.p.rapidapi.com"
-}
+def call_api():
+    # Headers used for RapidAPI.
+    headers = {
+        "X-RapidAPI-Key": rapid_api,
+        "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com"
+    }
 
-# Standings endpoint from RapidAPI.
-url = "https://api-football-v1.p.rapidapi.com/v3/standings"
+    # Standings endpoint from RapidAPI.
+    url = "https://api-football-v1.p.rapidapi.com/v3/standings"
 
-# Building query to retrieve data.
-query = {"season":"2022","league":"39"}
-response = requests.request("GET", url, headers=headers, params=query)
-json_res = response.json()
+    # Building query to retrieve data.
+    query = {"season":"2022","league":"39"}
+    response = requests.request("GET", url, headers=headers, params=query)
+    json_res = response.json()
 
-# Empty lists that will be filled and then used to create a dataframe.
-rank_list = []
-team_list = []
-wins_list = []
-draws_list = []
-loses_list = []
-points_list = []
-goals_for = []
-goals_against = []
-goals_diff = []
+    return json_res
 
-# Filling in empty lists.
-count = 0
-while count < 20:
-    # Team postion data.
-    rank_list.append(int(json.dumps(json_res
-        ["response"][0]["league"]["standings"][0][count]["rank"])))
+def standings():
+    json_res = call_api()
 
-    # Team names.
-    team_list.append(str(json.dumps(json_res
-        ["response"][0]["league"]["standings"][0][count]["team"]["name"])).strip('"'))
+    # Empty lists that will be filled and then used to create a dataframe.
+    rank_list = []
+    team_list = []
+    wins_list = []
+    draws_list = []
+    loses_list = []
+    points_list = []
+    goals_for = []
+    goals_against = []
+    goals_diff = []
 
-    # Number of wins.
-    wins_list.append(int(json.dumps(json_res
-        ["response"][0]["league"]["standings"][0][count]["all"]["win"])))
+    # Filling in empty lists.
+    count = 0
+    while count < 20:
+        # Team postion data.
+        rank_list.append(int(json.dumps(json_res
+            ["response"][0]["league"]["standings"][0][count]["rank"])))
 
-    # Number of draws.
-    draws_list.append(int(json.dumps(json_res
-        ["response"][0]["league"]["standings"][0][count]["all"]["draw"])))
+        # Team names.
+        team_list.append(str(json.dumps(json_res
+            ["response"][0]["league"]["standings"][0][count]["team"]["name"])).strip('"'))
 
-    # Number of loses.
-    loses_list.append(int(json.dumps(json_res
-        ["response"][0]["league"]["standings"][0][count]["all"]["lose"])))
+        # Number of wins.
+        wins_list.append(int(json.dumps(json_res
+            ["response"][0]["league"]["standings"][0][count]["all"]["win"])))
 
-    # Number of points.
-    points_list.append(int(json.dumps(json_res
-        ["response"][0]["league"]["standings"][0][count]["points"])))
+        # Number of draws.
+        draws_list.append(int(json.dumps(json_res
+            ["response"][0]["league"]["standings"][0][count]["all"]["draw"])))
 
-    # Number of goals for.
-    goals_for.append(int(json.dumps(json_res
-        ["response"][0]["league"]["standings"][0][count]["all"]["goals"]["for"])))
+        # Number of loses.
+        loses_list.append(int(json.dumps(json_res
+            ["response"][0]["league"]["standings"][0][count]["all"]["lose"])))
 
-    # Number of goals against.
-    goals_against.append(int(json.dumps(json_res
-        ["response"][0]["league"]["standings"][0][count]["all"]["goals"]["against"])))
+        # Number of points.
+        points_list.append(int(json.dumps(json_res
+            ["response"][0]["league"]["standings"][0][count]["points"])))
 
-    # Number of goal differential.
-    goals_diff.append(int(json.dumps(json_res
-        ["response"][0]["league"]["standings"][0][count]["goalsDiff"])))
-    count += 1
+        # Number of goals for.
+        goals_for.append(int(json.dumps(json_res
+            ["response"][0]["league"]["standings"][0][count]["all"]["goals"]["for"])))
+
+        # Number of goals against.
+        goals_against.append(int(json.dumps(json_res
+            ["response"][0]["league"]["standings"][0][count]["all"]["goals"]["against"])))
+
+        # Number of goal differential.
+        goals_diff.append(int(json.dumps(json_res
+            ["response"][0]["league"]["standings"][0][count]["goalsDiff"])))
+        count += 1
+
+    return rank_list, team_list, wins_list, draws_list, loses_list, points_list, goals_for, goals_against, goals_diff
+
+def dataframe():
+	rank_list, team_list, wins_list, draws_list, loses_list, points_list, goals_for, goals_against, goals_diff = standings()
+
+	# Setting the headers then zipping the lists to create a dataframe.
+	headers = ['Rank', 'Team', 'Wins', 'Draws', 'Loses', 'Points', 'GF', 'GA', 'GD']
+	zipped = list(zip(rank_list, team_list, wins_list, draws_list, loses_list, points_list, goals_for, goals_against, goals_diff))
+
+	df = pd.DataFrame(zipped, columns=headers)
+
+	return df
 
 class Standings:
 
@@ -86,11 +105,7 @@ class Standings:
         print("Standings table dropped...")
 
     def load(self):
-        # Setting the headers then zipping the lists to create a dataframe.
-        headers = ['Rank', 'Team', 'Wins', 'Draws', 'Loses', 'Points', 'GF', 'GA', 'GD']
-        zipped = list(zip(rank_list, team_list, wins_list, draws_list, loses_list, points_list, goals_for, goals_against, goals_diff))
-
-        df = pd.DataFrame(zipped, columns=headers)
+        df = dataframe() # Getting dataframe creating in dataframe() function.
 
         # Construct a BigQuery client object.
         client = bigquery.Client(project=project_id)
