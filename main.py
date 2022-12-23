9dcda191dad33a904bbc6c82ae6cfd8a08ad12b9
SquareHammer89 <pythonsanchez@gmail.com>
2022-12-22 20:06:50 -0800

    adding Teams class

--- main.py
+++ main.py
@@ -3,18 +3,22 @@ import os
 
 os.environ[c]=a
 
+print("Process starting...")
+
 def stats():
     # Importing classes from src files.
-    from src.location import Location
+    from src.locations import Locations
     from src.players import Players
     from src.standings import Standings
+    from src.teams import Teams
 
-    location = Location()
+    locations = Locations()
     players = Players()
     standings = Standings()
+    teams = Teams()
     
     # Dropping and loading the standings and players dataframes.
-    return standings.drop(), standings.load(), players.drop(), players.load()
+    return players.drop(), players.load(), standings.drop(), standings.load(), teams.drop(), teams.load()
 
 if __name__ == "__main__":
     stats()
