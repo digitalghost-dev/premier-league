f85d274745b228fadd88457f3de413ce41d79614
Christian <pythonsanchez@gmail.com>
2023-08-09 20:04:25 -0700

    adding remaining tasks

--- prefect/flows.py
+++ prefect/flows.py
@@ -1,5 +1,19 @@
-from prefect import flow
+from prefect import task, flow
+
+@task
+def standings():
+    from etl.postgres.standings import send_dataframe_to_postgresql
+
+@task
+def teams():
+    from etl.postgres.teams import send_dataframe_to_postgresql
+
+@task
+def top_scorers():
+    from etl.postgres.top_scorers import send_dataframe_to_postgresql
 
 @flow
 def premier_league_flow():
-    from etl.postgres.standings import send_dataframe_to_postgresql
+    a = standings()
+    b = teams(wait_for=[a])
+    c = top_scorers(wait_for=[a, b])
