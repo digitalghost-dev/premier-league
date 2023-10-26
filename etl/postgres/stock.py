8452f25e9f8b210fa1ef5fc8d206a80f01d67cc9
Christian <pythonsanchez@gmail.com>
2023-10-25 20:25:47 -0700

    formatting with ruff

--- etl/postgres/stock.py
+++ etl/postgres/stock.py
@@ -9,42 +9,42 @@ os.environ["GCLOUD_PROJECT"] = "cloud-data-infrastructure"
 
 
 def gcp_secret_stock_api() -> str:
-    """This function retrieves the Rapid API key from GCP Secret Manager"""
+	"""This function retrieves the Rapid API key from GCP Secret Manager"""
 
-    client = secretmanager.SecretManagerServiceClient()
-    name = "projects/463690670206/secrets/stock-api/versions/1"
-    response = client.access_secret_version(request={"name": name})
-    stock_api_key = response.payload.data.decode("UTF-8")
+	client = secretmanager.SecretManagerServiceClient()
+	name = "projects/463690670206/secrets/stock-api/versions/1"
+	response = client.access_secret_version(request={"name": name})
+	stock_api_key = response.payload.data.decode("UTF-8")
 
-    return stock_api_key
+	return stock_api_key
 
 
 def gcp_secret_postgresql_uri() -> str:
-    """This function retrieves the Rapid API key from GCP Secret Manager"""
+	"""This function retrieves the Rapid API key from GCP Secret Manager"""
 
-    client = secretmanager.SecretManagerServiceClient()
-    name = "projects/463690670206/secrets/postgresql-uri/versions/1"
-    response = client.access_secret_version(request={"name": name})
-    postgresql_uri = response.payload.data.decode("UTF-8")
+	client = secretmanager.SecretManagerServiceClient()
+	name = "projects/463690670206/secrets/postgresql-uri/versions/1"
+	response = client.access_secret_version(request={"name": name})
+	postgresql_uri = response.payload.data.decode("UTF-8")
 
-    return postgresql_uri
+	return postgresql_uri
 
 
 def send_dataframe_to_postgres() -> None:
-    stock_api_key = gcp_secret_stock_api()
-    postgresql_uri = gcp_secret_postgresql_uri()
+	stock_api_key = gcp_secret_stock_api()
+	postgresql_uri = gcp_secret_postgresql_uri()
 
-    url = f"https://financialmodelingprep.com/api/v3/quote/MANU?apikey={stock_api_key}"
+	url = f"https://financialmodelingprep.com/api/v3/quote/MANU?apikey={stock_api_key}"
 
-    response = requests.request("GET", url)
+	response = requests.request("GET", url)
 
-    json_res = response.json()
-    df = pl.DataFrame(json_res)
+	json_res = response.json()
+	df = pl.DataFrame(json_res)
 
-    df.write_database(
-        table_name="stocks", connection=postgresql_uri, if_exists="append"
-    )
+	df.write_database(
+		table_name="stocks", connection=postgresql_uri, if_exists="append"
+	)
 
 
 if __name__ != "__main__":
-    send_dataframe_to_postgres()
+	send_dataframe_to_postgres()
