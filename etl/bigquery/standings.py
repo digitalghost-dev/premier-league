26782420bb5d7f63cb896dbe7e8c5289b17e5a41
Christian <pythonsanchez@gmail.com>
2023-12-10 18:28:44 -0800

    adding 'games_played' to table

--- etl/bigquery/standings.py
+++ etl/bigquery/standings.py
@@ -17,267 +17,284 @@ os.environ["GCLOUD_PROJECT"] = "cloud-data-infrastructure"
 
 
 def gcp_secret_rapid_api() -> str:
-    """This function retrieves the Rapid API key from GCP Secret Manager"""
+	"""This function retrieves the Rapid API key from GCP Secret Manager"""
 
-    client = secretmanager.SecretManagerServiceClient()
-    name = "projects/463690670206/secrets/rapid-api/versions/1"
-    response = client.access_secret_version(request={"name": name})
-    rapid_api_key = response.payload.data.decode("UTF-8")
+	client = secretmanager.SecretManagerServiceClient()
+	name = "projects/463690670206/secrets/rapid-api/versions/1"
+	response = client.access_secret_version(request={"name": name})
+	rapid_api_key = response.payload.data.decode("UTF-8")
 
-    return rapid_api_key
+	return rapid_api_key
 
 
 def call_api() -> (
-    tuple[
-        list[int],
-        list[int],
-        list[str],
-        list[int],
-        list[int],
-        list[int],
-        list[str],
-        list[int],
-        list[int],
-        list[int],
-        list[int],
-        list[str],
-        list[str],
-    ]
+	tuple[
+		list[int],
+		list[int],
+		list[str],
+		list[int],
+		list[int],
+		list[int],
+		list[int],
+		list[str],
+		list[int],
+		list[int],
+		list[int],
+		list[int],
+		list[str],
+		list[str],
+	]
 ):
-    """This function calls the API then fills in the empty lists"""
+	"""This function calls the API then fills in the empty lists"""
 
-    payload = gcp_secret_rapid_api()
-    # Headers used for RapidAPI.
-    headers = {
-        "X-RapidAPI-Key": payload,
-        "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com",
-    }
+	payload = gcp_secret_rapid_api()
 
-    # Standings endpoint from RapidAPI.
-    url = "https://api-football-v1.p.rapidapi.com/v3/standings"
+	# Headers used for RapidAPI.
+	headers = {
+		"X-RapidAPI-Key": payload,
+		"X-RapidAPI-Host": "api-football-v1.p.rapidapi.com",
+	}
 
-    # Building GET request to retrieve data.
-    query = {"season": "2023", "league": "39"}
-    response = requests.get(url, headers=headers, params=query, timeout=10)
-    json_res = response.json()
+	# Standings endpoint from RapidAPI.
+	url = "https://api-football-v1.p.rapidapi.com/v3/standings"
 
-    # Empty lists that will be filled and then used to create a dataframe.
-    team_id_list = []
-    rank_list = []
-    team_list = []
-    wins_list = []
-    draws_list = []
-    loses_list = []
-    form_list = []
-    points_list = []
-    goals_for = []
-    goals_against = []
-    goals_diff = []
-    status_list = []
-    logo_list = []
+	# Building GET request to retrieve data.
+	query = {"season": "2023", "league": "39"}
+	response = requests.get(url, headers=headers, params=query, timeout=10)
+	json_res = response.json()
 
-    # Filling in empty lists.
-    count = 0
-    while count < 20:
-        # Team ID.
-        team_id_list.append(
-            int(json_res["response"][0]["league"]["standings"][0][count]["team"]["id"])
-        )
+	# Empty lists that will be filled and then used to create a dataframe.
+	team_id_list = []
+	rank_list = []
+	team_list = []
+	games_played = []
+	wins_list = []
+	draws_list = []
+	loses_list = []
+	form_list = []
+	points_list = []
+	goals_for = []
+	goals_against = []
+	goals_diff = []
+	status_list = []
+	logo_list = []
 
-        # Team rank.
-        rank_list.append(
-            int(json_res["response"][0]["league"]["standings"][0][count]["rank"])
-        )
+	# Filling in empty lists.
+	count = 0
+	while count < 20:
+		# Team ID.
+		team_id_list.append(
+			int(json_res["response"][0]["league"]["standings"][0][count]["team"]["id"])
+		)
 
-        # Team names.
-        team_list.append(
-            str(
-                json.dumps(
-                    json_res["response"][0]["league"]["standings"][0][count]["team"][
-                        "name"
-                    ]
-                )
-            ).strip('"')
-        )
+		# Team rank.
+		rank_list.append(
+			int(json_res["response"][0]["league"]["standings"][0][count]["rank"])
+		)
 
-        # Number of wins.
-        wins_list.append(
-            int(json_res["response"][0]["league"]["standings"][0][count]["all"]["win"])
-        )
+		# Team names.
+		team_list.append(
+			str(
+				json.dumps(
+					json_res["response"][0]["league"]["standings"][0][count]["team"][
+						"name"
+					]
+				)
+			).strip('"')
+		)
 
-        # Number of draws.
-        draws_list.append(
-            int(json_res["response"][0]["league"]["standings"][0][count]["all"]["draw"])
-        )
+		# Amount of games played.
+		games_played.append(
+			int(
+				json_res["response"][0]["league"]["standings"][0][count]["all"][
+					"played"
+				]
+			)
+		)
 
-        # Number of loses.
-        loses_list.append(
-            int(json_res["response"][0]["league"]["standings"][0][count]["all"]["lose"])
-        )
+		# Number of wins.
+		wins_list.append(
+			int(json_res["response"][0]["league"]["standings"][0][count]["all"]["win"])
+		)
 
-        # Team forms.
-        form_list.append(
-            str(
-                json.dumps(
-                    json_res["response"][0]["league"]["standings"][0][count]["form"]
-                )
-            ).strip('"')
-        )
+		# Number of draws.
+		draws_list.append(
+			int(json_res["response"][0]["league"]["standings"][0][count]["all"]["draw"])
+		)
 
-        # Number of points.
-        points_list.append(
-            int(json_res["response"][0]["league"]["standings"][0][count]["points"])
-        )
+		# Number of loses.
+		loses_list.append(
+			int(json_res["response"][0]["league"]["standings"][0][count]["all"]["lose"])
+		)
 
-        # Number of goals for.
-        goals_for.append(
-            int(
-                json_res["response"][0]["league"]["standings"][0][count]["all"][
-                    "goals"
-                ]["for"]
-            )
-        )
+		# Team forms.
+		form_list.append(
+			str(
+				json.dumps(
+					json_res["response"][0]["league"]["standings"][0][count]["form"]
+				)
+			).strip('"')
+		)
 
-        # Number of goals against.
-        goals_against.append(
-            int(
-                json_res["response"][0]["league"]["standings"][0][count]["all"][
-                    "goals"
-                ]["against"]
-            )
-        )
+		# Number of points.
+		points_list.append(
+			int(json_res["response"][0]["league"]["standings"][0][count]["points"])
+		)
 
-        # Number of goal differential.
-        goals_diff.append(
-            int(json_res["response"][0]["league"]["standings"][0][count]["goalsDiff"])
-        )
+		# Number of goals for.
+		goals_for.append(
+			int(
+				json_res["response"][0]["league"]["standings"][0][count]["all"][
+					"goals"
+				]["for"]
+			)
+		)
 
-        # Each team's position status.
-        status_list.append(
-            str(json_res["response"][0]["league"]["standings"][0][count]["status"])
-        )
+		# Number of goals against.
+		goals_against.append(
+			int(
+				json_res["response"][0]["league"]["standings"][0][count]["all"][
+					"goals"
+				]["against"]
+			)
+		)
 
-        # Team's logo.
-        logo_list.append(
-            str(
-                json_res["response"][0]["league"]["standings"][0][count]["team"]["logo"]
-            )
-        )
+		# Number of goal differential.
+		goals_diff.append(
+			int(json_res["response"][0]["league"]["standings"][0][count]["goalsDiff"])
+		)
 
-        count += 1
+		# Each team's position status.
+		status_list.append(
+			str(json_res["response"][0]["league"]["standings"][0][count]["status"])
+		)
 
-    return (
-        team_id_list,
-        rank_list,
-        team_list,
-        wins_list,
-        draws_list,
-        loses_list,
-        form_list,
-        points_list,
-        goals_for,
-        goals_against,
-        goals_diff,
-        status_list,
-        logo_list,
-    )
+		# Team's logo.
+		logo_list.append(
+			str(
+				json_res["response"][0]["league"]["standings"][0][count]["team"]["logo"]
+			)
+		)
+
+		count += 1
+
+	return (
+		team_id_list,
+		rank_list,
+		team_list,
+		games_played,
+		wins_list,
+		draws_list,
+		loses_list,
+		form_list,
+		points_list,
+		goals_for,
+		goals_against,
+		goals_diff,
+		status_list,
+		logo_list,
+	)
 
 
 def create_dataframe() -> DataFrame:
-    """This function creates a datafreame from lists created in the last function: call_api()"""
+	"""This function creates a datafreame from lists created in the last function: call_api()"""
 
-    (
-        team_id_list,
-        rank_list,
-        team_list,
-        wins_list,
-        draws_list,
-        loses_list,
-        form_list,
-        points_list,
-        goals_for,
-        goals_against,
-        goals_diff,
-        status_list,
-        logo_list,
-    ) = call_api()
+	(
+		team_id_list,
+		rank_list,
+		team_list,
+		games_played,
+		wins_list,
+		draws_list,
+		loses_list,
+		form_list,
+		points_list,
+		goals_for,
+		goals_against,
+		goals_diff,
+		status_list,
+		logo_list,
+	) = call_api()
 
-    # Setting the headers then zipping the lists to create a dataframe.
-    headers = [
-        "team_id",
-        "rank",
-        "team",
-        "wins",
-        "draws",
-        "loses",
-        "recent_form",
-        "points",
-        "goals_for",
-        "goals_against",
-        "goal_difference",
-        "position_status",
-        "logo",
-    ]
-    zipped = list(
-        zip(
-            team_id_list,
-            rank_list,
-            team_list,
-            wins_list,
-            draws_list,
-            loses_list,
-            form_list,
-            points_list,
-            goals_for,
-            goals_against,
-            goals_diff,
-            status_list,
-            logo_list,
-        )
-    )
+	# Setting the headers then zipping the lists to create a dataframe.
+	headers = [
+		"team_id",
+		"rank",
+		"team",
+		"games_played",
+		"wins",
+		"draws",
+		"loses",
+		"recent_form",
+		"points",
+		"goals_for",
+		"goals_against",
+		"goal_difference",
+		"position_status",
+		"logo",
+	]
+	zipped = list(
+		zip(
+			team_id_list,
+			rank_list,
+			team_list,
+			games_played,
+			wins_list,
+			draws_list,
+			loses_list,
+			form_list,
+			points_list,
+			goals_for,
+			goals_against,
+			goals_diff,
+			status_list,
+			logo_list,
+		)
+	)
 
-    df = pd.DataFrame(zipped, columns=headers)
+	df = pd.DataFrame(zipped, columns=headers)
 
-    return df
+	return df
 
 
 def define_table_schema() -> list[dict[str, str]]:
-    """This function defines the schema for the table in BigQuery"""
+	"""This function defines the schema for the table in BigQuery"""
 
-    schema_definition = [
-        {"name": "team_id", "type": "INTEGER"},
-        {"name": "rank", "type": "INTEGER"},
-        {"name": "team", "type": "STRING"},
-        {"name": "wins", "type": "INTEGER"},
-        {"name": "draws", "type": "INTEGER"},
-        {"name": "loses", "type": "INTEGER"},
-        {"name": "recent_form", "type": "STRING"},
-        {"name": "points", "type": "STRING"},
-        {"name": "goals_for", "type": "INTEGER"},
-        {"name": "goals_against", "type": "INTEGER"},
-        {"name": "goal_difference", "type": "INTEGER"},
-        {"name": "position_status", "type": "STRING"},
-        {"name": "logo", "type": "STRING"},
-    ]
+	schema_definition = [
+		{"name": "team_id", "type": "INTEGER"},
+		{"name": "rank", "type": "INTEGER"},
+		{"name": "team", "type": "STRING"},
+		{"name": "games_played", "type": "INTEGER"},
+		{"name": "wins", "type": "INTEGER"},
+		{"name": "draws", "type": "INTEGER"},
+		{"name": "loses", "type": "INTEGER"},
+		{"name": "recent_form", "type": "STRING"},
+		{"name": "points", "type": "STRING"},
+		{"name": "goals_for", "type": "INTEGER"},
+		{"name": "goals_against", "type": "INTEGER"},
+		{"name": "goal_difference", "type": "INTEGER"},
+		{"name": "position_status", "type": "STRING"},
+		{"name": "logo", "type": "STRING"},
+	]
 
-    return schema_definition
+	return schema_definition
 
 
 def send_dataframe_to_bigquery(
-    standings_dataframe: DataFrame, schema_definition: list[dict[str, str]]
+	standings_dataframe: DataFrame, schema_definition: list[dict[str, str]]
 ) -> None:
-    """This function sends the dataframe to BigQuery"""
+	"""This function sends the dataframe to BigQuery"""
 
-    standings_dataframe.to_gbq(
-        destination_table="premier_league_dataset.standings",
-        if_exists="replace",
-        table_schema=schema_definition,
-    )
+	standings_dataframe.to_gbq(
+		destination_table="premier_league_dataset.standings",
+		if_exists="replace",
+		table_schema=schema_definition,
+	)
 
-    print("Standings table loaded!")
+	print("Standings table loaded!")
 
 
 if __name__ != "__main__":
-    standings_dataframe = create_dataframe()
-    schema_definition = define_table_schema()
-    send_dataframe_to_bigquery(standings_dataframe, schema_definition)
+	standings_dataframe = create_dataframe()
+	schema_definition = define_table_schema()
+	send_dataframe_to_bigquery(standings_dataframe, schema_definition)
