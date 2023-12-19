7ce304f69282834b0a2b60cadcbea5dec8b91b05
Christian <pythonsanchez@gmail.com>
2023-12-19 07:00:33 -0800

    adding header tests for all tabs

--- tests/test_streamlit_app.py
+++ tests/test_streamlit_app.py
@@ -9,12 +9,30 @@ def test_title_area():
 	assert "Current Round: " in at.subheader[0].value
 
 
+# Standings & Overivew
 def test_tab_one():
 	assert at.tabs[0].subheader[0].value == "League Statistics"
 	assert at.tabs[0].subheader[1].value == "Current Standings"
 	assert at.tabs[0].subheader[2].value == "Location of Stadiums"
 
 
+# Top Teams & Scorers
 def test_tab_two():
 	assert at.tabs[1].subheader[0].value == "Top 5 Teams"
 	assert at.tabs[1].subheader[2].value == "Point Progression throughout the Season"
+
+
+# Fixtures
+def test_tab_three():
+	assert at.tabs[2].header[0].value == "Fixtures"
+
+
+# News & Highlights
+def test_tab_four():
+	assert at.tabs[3].header[0].value == "Recent News"
+	assert at.tabs[3].header[1].value == "Recent Highlights"
+
+
+# About
+def test_tab_five():
+	assert at.tabs[4].header[0].value == "About"
