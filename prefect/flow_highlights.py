ffb392cc9a7b2f27a3a126ce73ca376957c67f1c
Christian <pythonsanchez@gmail.com>
2024-01-02 14:35:21 -0800

    removing prefect flow files, will store in one single file

--- prefect/flow_highlights.py
+++
@@ -1,9 +0,0 @@
-from prefect import task, flow
-
-@task
-def task_highlights():
-    from etl.bigquery.highlights import send_dataframe_to_bigquery
-
-@flow
-def highlights():
-    a = task_highlights()
